0a0873cd2e57440ab5f2e148b5af09068fcd38ed
Dimas Febriyanto <dimasfebriyanto@pop-os.localdomain>
2023-08-23 19:42:15 +0700

    add: Page admin movie

--- src/pages/AdminMovie.tsx
+++ src/pages/AdminMovie.tsx
@@ -1,17 +1,27 @@
 import AdminNavBar from "./Admin.tsx";
 import axios from "axios";
 import {useEffect, useState} from "react";
+import ShowDialogAddMovie from "../components/ShowDialogAddMovie.tsx";
+import {TypeOperation} from "../types/TypeOperation.tsx";
+import ShowDialogDeleteMovie from "../components/ShowDialogDeleteMovie.tsx";
 
 const AdminMovie = () => {
 
     const [movies, setMovies] = useState<MoviesType[] | null>(null);
-    const [genreDataFetched, setGenreDataFetched] = useState(false);
+    const [genreDataFetched, setGenreDataFetched] = useState<boolean>(false);
+    const [open, setOpen] = useState<boolean>(false);
+    const [typeOperation, setTypeOperation] = useState<TypeOperation>(TypeOperation.Add);
+    const [movie, setMovie] = useState<MoviesType | null>(null);
+
+    const [openDelete, setOpenDelete] = useState<boolean>(false);
 
     const fetchDataMovies = async () => {
         try {
             const APIURL = import.meta.env.VITE_URL_API;
             const response = await axios.get(`${APIURL}/movies`)
-            setMovies(response.data.data);
+            const data = response.data.data;
+            const sortedMovies = [...data].sort((a, b) => b.id - a.id);
+            setMovies(sortedMovies);
         } catch (e) {
             console.log(e)
         }
@@ -39,6 +49,30 @@ const AdminMovie = () => {
         }
     }
 
+    const handleOpenDelete = (movie: MoviesType) => {
+        setOpenDelete(!openDelete);
+        setMovie(movie);
+    }
+
+    const handleCloseAdd = () => {
+        setOpen(false);
+    }
+
+    const handleCloseDelete = () => {
+        setOpenDelete(false);
+    }
+
+    const handleOpenAdd = () => {
+        setOpen(!open);
+        setTypeOperation(TypeOperation.Add)
+    }
+
+    const handleOpenUpdate = (movie: MoviesType) => {
+        setMovie(movie)
+        setOpen(!open);
+        setTypeOperation(TypeOperation.Update)
+    }
+
     useEffect(() => {
         fetchDataMovies();
     }, [])
@@ -49,11 +83,20 @@ const AdminMovie = () => {
         }
     }, [movies]);
 
+    useEffect(() => {
+        if (!open && !openDelete) {
+            setMovie(null);
+        }
+    }, []);
 
-    return <div className="w-full h-screen flex justify-center bg-teal-500">
+
+    return <div className="w-full flex justify-center">
         <div className="w-full bg-white">
             <AdminNavBar/>
+            <ShowDialogDeleteMovie open={openDelete} handleOpen={handleCloseDelete} movieId={movie?.id}/>
+            <ShowDialogAddMovie open={open} handleOpen={handleCloseAdd} type={typeOperation} movie={movie}/>
             <button
+                onClick={handleOpenAdd}
                 className="flex items-center mx-5 my-3 justify-center w-1/2 px-5 py-2 text-sm
                 tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
@@ -61,7 +104,6 @@ const AdminMovie = () => {
                     <path strokeLinecap="round" strokeLinejoin="round"
                           d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
                 </svg>
-
                 <span>Add Film</span>
             </button>
             <div className="rounded-lg border border-gray-200 shadow-md m-5 overflow-x-scroll">
@@ -90,9 +132,14 @@ const AdminMovie = () => {
                                             alt=""
                                         />
                                     </div>
-                                    <div className="text-sm">
-                                        <div className="font-medium text-gray-700">{movie.title}</div>
-                                        <div className="text-blue-500"><a href={movie.trailer_url}>Trailer</a></div>
+                                    <div className="text-sm flex flex-col justify-between">
+                                        <div>
+                                            <div
+                                                className="font-medium text-gray-700 w-max">{movie.title}</div>
+                                            <div className="text-blue-500"><a href={movie.trailer_url}>Trailer</a></div>
+                                        </div>
+                                        <div
+                                            className="bg-secondaryColor text-white w-max px-2 rounded text-xs">ID {movie.id}</div>
                                     </div>
                                 </th>
                                 <td className="px-6 py-4">
@@ -112,7 +159,7 @@ const AdminMovie = () => {
                                 </td>
                                 <td className="px-6 py-4">
                                     <div className="flex justify-end gap-4">
-                                        <a href="#">
+                                        <button onClick={() => handleOpenDelete(movie)}>
                                             <svg
                                                 xmlns="http://www.w3.org/2000/svg"
                                                 fill="none"
@@ -127,8 +174,8 @@ const AdminMovie = () => {
                                                     d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
                                                 />
                                             </svg>
-                                        </a>
-                                        <a href="#">
+                                        </button>
+                                        <button onClick={() => handleOpenUpdate(movie)}>
                                             <svg
                                                 xmlns="http://www.w3.org/2000/svg"
                                                 fill="none"
@@ -143,7 +190,7 @@ const AdminMovie = () => {
                                                     d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
                                                 />
                                             </svg>
-                                        </a>
+                                        </button>
                                     </div>
                                 </td>
                             </tr>;
